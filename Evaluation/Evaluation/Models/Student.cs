c5c0c563937a03492777eee33b51c108a2bdda3a
Sebastian Morales Yabeta <masterrol001@hotmail.com>
2019-09-27 16:47:25 -0400

    2do Commit Modelos y Base de datos local

---
+++ Evaluation/Evaluation/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Evaluation.Models
+{
+    public enum TypeGender { Male, Female, Helicopter, Other }
+
+    public class Student
+    {
+        [Key]
+        public string Name { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Birthdate { get; set; }
+        [Required]
+        public int MyProperty { get; set; }
+    }
+}
